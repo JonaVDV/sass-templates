679359e13f8986b4ad18fc292828e5f347b8c13d
jvdv <j.vdv@outlook.com>
2023-08-25 10:51:10 +0200

    add a type to the astro config

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 import purgecss from "astro-purgecss";
 
 // https://astro.build/config
+/**@type {import('astro').AstroUserConfig}*/
 export default defineConfig({
   integrations: [purgecss({
     content: [
       process.cwd() + '/src/**/*.{astro,svelte,vue,react}',
     ],
-  })]
+  })],
 });

44a9b698c2ef1f6a03902b1ca21856cf3d2ba4b2
Jona_van_der_Vlist-1075709 <j.vdv@outlook.com>
2024-01-23 22:32:02 +0100

    WIP: add logic for dialog animations

--- vanilla-js/src/main.ts
+++ vanilla-js/src/main.ts
@@ -1,2 +1,36 @@
 // importing scss here so it gets compiled by vite
 import './scss/main.scss'
+
+
+const modals = document.querySelectorAll('dialog')
+const modalTriggers = document.querySelectorAll('[data-modal]')
+
+modalTriggers.forEach(trigger => {
+  trigger.addEventListener('click', async () => {
+    const modal = document.getElementById(trigger.getAttribute('data-modal') || '')
+    if (modal && modal instanceof HTMLDialogElement) {
+      // await all animations to finish
+      await awaitAnimations(modal).then(() => {
+        modal.showModal()
+      })
+
+      modal.addEventListener('close', () => {
+        closeModal(modal)
+      })
+    }
+  })
+})
+
+async function closeModal(modal: HTMLDialogElement){
+  // modal.open = true
+  modal.setAttribute('closing', '')
+  await awaitAnimations(modal).then(() => {
+    modal.close()
+    modal.removeAttribute('closing')
+  })
+} 
+
+
+function awaitAnimations(element: Element) {
+  return Promise.all(element.getAnimations().map(animation => animation.finished))
+}
